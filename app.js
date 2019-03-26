7aace37a4e54cb20e200e94d2898cd0292b77b90
lpmattd <matt.dittmann@logicalposition.com>
2019-03-26 11:06:32 -0700

    add project files

---
+++ app.js
@@ -0,0 +1,54 @@
+const cards = [
+    {
+      front: 'The "First Computer Programmer"',
+      back: 'Ada Lovelace',
+      flipped: false,
+    },
+    {
+      front: 'Invented the "Clarke Calculator"',
+      back: 'Edith Clarke',
+      flipped: false,
+  
+    },
+    {
+      front: 'Famous World War II Enigma code breaker',
+      back: 'Alan Turing',
+      flipped: false,
+    },
+    {
+      front: 'Created satellite orbit analyzation software for NASA',
+      back: 'Dr. Evelyn Boyd Granville',
+      flipped: false,
+    },
+  ]; 
+
+  // Animate card flip
+  // Display error message if form fields are blank
+  new Vue({
+    el: '#flashcard-app',
+    data: {
+      cards: cards,
+      newFront: '',
+      newBack: '',
+      error: false
+    },
+    methods: {
+      toggleCard: function(card) {
+        card.flipped = !card.flipped;
+      },
+      addNew: function() {
+        if(!this.newFront || !this.newBack) {
+          this.error = true;
+        } else {
+          this.error = false;
+          this.cards.push({
+            front: this.newFront,
+            back: this.newBack,
+            flipped: false
+          });
+          this.newFront = '';
+          this.newBack = '';
+        }
+      }
+    }
+  });
